6091beb3bf7d8460dc8f51a5b6fe1bd2afee1979
Manuel Pérez <manuel.perez@tech.better.team>
2025-02-26 13:18:37 +0100

    debugging minimal

--- book_classes.py
+++ book_classes.py
@@ -21,31 +21,30 @@ now = datetime.now(tz=config.load_timezone())
 today = now.strftime('%Y-%m-%d')
 in_a_week = (now + timedelta(days=7)).strftime('%Y-%m-%d')
 
-# # Setup the matcher to establish whether a class is of interest
-# classEventMatcher = config.create_matcher()
-#
-#
-# # Query for the interesting events the upcoming week
-# interesting_events = [event for event in session.search_events(config.facilityId, today, in_a_week) if event.bookingInfo.bookingAvailable and classEventMatcher.matches(event)]
-# for event in interesting_events:
-#     print(f"Found event matching interests: {event.to_string_with_status()}")
-#
-# # Filter down to those that we can book
-# filtered_events = [event for event in interesting_events
-#                    if not event.is_signed_up()
-#                    and not event.signup_closed()]
-#
-# # Get just the closest bookable one
-# if filtered_events:
-#     bookable_events = [min(filtered_events, key=lambda event: event.startDate)]
-# else:
-#     bookable_events = []
-#     print("No bookable events found.")
-# print()
-# for event in bookable_events:
-#     print(f"Found bookable event: {event.to_string_with_status()}")
-#     print()
-#
+# Setup the matcher to establish whether a class is of interest
+classEventMatcher = config.create_matcher()
+
+# Query for the interesting events the upcoming week
+interesting_events = [event for event in session.search_events(config.facilityId, today, in_a_week) if event.bookingInfo.bookingAvailable and classEventMatcher.matches(event)]
+for event in interesting_events:
+    print(f"Found event matching interests: {event.to_string_with_status()}")
+
+# Filter down to those that we can book
+filtered_events = [event for event in interesting_events
+                   if not event.is_signed_up()
+                   and not event.signup_closed()]
+
+# Get just the closest bookable one
+if filtered_events:
+    bookable_events = [min(filtered_events, key=lambda event: event.startDate)]
+else:
+    bookable_events = []
+    print("No bookable events found.")
+print()
+for event in bookable_events:
+    print(f"Found bookable event: {event.to_string_with_status()}")
+    print()
+
 # # Suspends the current thread until the supplied time,
 # def pause_until(pause_time):
 #     tz = pytz.timezone('Europe/Berlin')  # Replace with your timezone
